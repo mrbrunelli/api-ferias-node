0eeecfeddb687976d3fc617d3f8a4c6cf81f7f63
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-05-12 15:35:03 -0300

    Adicionando index e controller dos colaboradores .

---
+++ src/controllers/ColaboradorController.js
@@ -0,0 +1,8 @@
+/**
+ * Arquivo: ColaboradorController.js
+ * Descrição: Responsável por gerenciar os colaboradores (Cadastrar, Listar, Atualizar e Deletar Colaboradores)
+ * Data: 12/05/2020
+ * Autor: Matheus Ricardo Brunelli
+ */
+
+const db = require('../config/database')
