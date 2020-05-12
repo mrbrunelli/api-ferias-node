5f897a2602fbe0f01cd69a4f6a5b6f53a4a2a704
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-05-12 15:38:23 -0300

    Adicionando rotas.

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+/**
+ * Arquivo: routes.js
+ * Descrição: Gerenciar rotas da aplicação
+ * Data: 12/05/2020
+ * Autor: Matheus Ricardo Brunelli
+ */
+
+const express = require('express')
+
+const routes = express.Router()
+
+module.exports = routes
